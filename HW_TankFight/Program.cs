70751eda5b15374c6dbb6377a167e99ba4f7e0ba
Жанара <@DESKTOP-O9KC10I>
2019-02-26 20:15:21 +0600

    Add project files.

---
+++ HW_TankFight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW_TankFight.World_of_Tanks;
+namespace HW_TankFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] T_34 = new Tank[5];
+            T_34[0] = new Tank("T_34-1");
+            T_34[1] = new Tank("T_34-2");
+            T_34[2] = new Tank("T_34-3");
+            T_34[3] = new Tank("T_34-4");
+            T_34[4] = new Tank("T_34-5");
+            Tank[] Pantera = new Tank[5];
+            Pantera[0] = new Tank("Пантера-1");
+            Pantera[1] = new Tank("Пантера-2");
+            Pantera[2] = new Tank("Пантера-3");
+            Pantera[3] = new Tank("Пантера-4");
+            Pantera[4] = new Tank("Пантера-5");
+            for (int i = 0; i < 5; i++)
+            {
+                if ((T_34[i] * Pantera[i]) == true)
+                {
+                    System.Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Победили Т_34");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                else
+                {
+                    System.Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Победили Пантеры");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ HW_TankFight/World of Tanks/Tank.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_TankFight.World_of_Tanks
+{
+    class Tank
+    {
+        private string Name;
+        private int Ammunition;
+        private int Armor;
+        private int Maneuverability;
+
+        public static Random rnd = new Random();
+
+        public Tank(string name)
+        {
+            this.Name = name;
+            this.Ammunition = (int)rnd.Next(0, 100);
+            this.Armor= (int)rnd.Next(0, 100);
+            this.Maneuverability= (int)rnd.Next(0, 100);
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+        public string GetAmmun()
+        {
+            string res = string.Format("Боекомплект: {0}%", Ammunition);
+            return res;
+        }
+        public string GetArmor()
+        {
+            string res = string.Format("Боекомплект: {0}%", Armor);
+            return res;
+        }
+        public string GetManeu()
+        {
+            string res = string.Format("Боекомплект: {0}%", Maneuverability);
+            return res;
+        }
+        public string GetInfo()
+        {
+            string res = string.Format("Боекомплект: {0}%\nУроень брони: {1}%\nУровень маневренности: {2}%", Ammunition, Armor, Maneuverability);
+            return res;
+        }
+
+        public static bool operator *(Tank t1, Tank t2)
+        {
+            int t1_count = 0, t2_count = 0;
+            if (t1.Ammunition > t2.Ammunition)
+                t1_count++;
+            else
+                t2_count++;
+            if (t1.Armor > t2.Armor)
+                t1_count++;
+            else
+                t2_count++;
+            if (t1.Maneuverability > t2.Maneuverability)
+                t1_count++;
+            else
+                t2_count++;
+            if (t1_count > t2_count)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+}
